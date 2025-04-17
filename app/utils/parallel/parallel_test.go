0c50ca4e5915fe8c9c6f74c71469878a24177053
josephbarnett <joe.barnett@cloudzero.com>
2025-04-17 18:42:24 -0400

    move parallization helper to utils

--- app/utils/parallel/parallel_test.go
+++ app/utils/parallel/parallel_test.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/cloudzero/cloudzero-agent/app/parallel"
+	"github.com/cloudzero/cloudzero-agent/app/utils/parallel"
 )
 
 func TestNew(t *testing.T) {
