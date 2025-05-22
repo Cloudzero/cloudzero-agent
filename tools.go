e80dbcbb61ea4c35c497d37e8971793e3592f642
Evan Nemerson <evan.nemerson@cloudzero.com>
2025-05-22 15:22:27 -0400

    CP-29278: add Helm to tools.go
    Helm 3.18.0 includes a regression in the way values are written, which
    is causing our generated manifests to include an unwanted change when
    regenerated from with 3.18.0 (i.e., our laptops if we did a brew
    upgrade since 3.18.0).

--- tools.go
+++ tools.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/itchyny/gojq/cmd/gojq"
 	_ "go.uber.org/mock/mockgen"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	_ "helm.sh/helm/v3/cmd/helm"
 	_ "honnef.co/go/tools/cmd/staticcheck"
 	_ "mvdan.cc/gofumpt"
 )
