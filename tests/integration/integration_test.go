3113f434e72fca88b99dd34c4c18f128f330c19c
evan-cz <evan.nemerson@cloudzero.com>
2025-06-18 17:11:36 -0400

    CP-29743: update copyright year to 2025 (#299)

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -1,7 +1,7 @@
 //go:build integration
 // +build integration
 
-// SPDX-FileCopyrightText: Copyright (c) 2016-2024, CloudZero, Inc. or its affiliates. All Rights Reserved.
+// SPDX-FileCopyrightText: Copyright (c) 2016-2025, CloudZero, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 package integration
