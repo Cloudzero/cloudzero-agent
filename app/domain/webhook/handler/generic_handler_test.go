d596b2aa3573ecf29cb52575d81a90dce105f58c
Evan Nemerson <evan.nemerson@cloudzero.com>
2025-06-18 15:30:47 -0400

    CP-29743: update copyright year to 2025

--- app/domain/webhook/handler/generic_handler_test.go
+++ app/domain/webhook/handler/generic_handler_test.go
@@ -1,4 +1,4 @@
-// SPDX-License-IdentifierText: Copyright (c) 2016-2024, CloudZero, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-IdentifierText: Copyright (c) 2016-2025, CloudZero, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 package handler_test
