c4c83177464940e9fe0070b7a5784a24a71d9f59
josephbarnett <joe.barnett@cloudzero.com>
2025-07-11 00:13:26 -0400

    fix lint issue

--- tests/backfiller/mock_collector.go
+++ tests/backfiller/mock_collector.go
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright (c) 2016-2025, CloudZero, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// Package backfiller provides utilities for testing label backfiller integrations.
 package backfiller
 
 import (
