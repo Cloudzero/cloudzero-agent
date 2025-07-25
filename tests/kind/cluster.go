b1e4e07f6136418226f053869eea253e718e0623
josephbarnett <joe.barnett@cloudzero.com>
2025-07-25 01:12:05 -0400

    fix analyze

--- tests/kind/cluster.go
+++ tests/kind/cluster.go
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright (c) 2016-2025, CloudZero, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// Package kind provides utilities for managing Kind clusters in tests.
 package kind
 
 import (
